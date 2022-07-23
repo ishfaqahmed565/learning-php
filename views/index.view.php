6c8579b00fb6d7d8a134c1186c56772bc2d69675
ishfaq ahmed <ahmedishfaq737@gmail.com>
2022-07-23 17:04:01 +0600

    Used partials to load up different elemets of the  each view

--- views/index.view.php
+++ views/index.view.php
@@ -1,34 +1,23 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-    <style>
-  
-    </style>
-</head>
-
-<body>
-    <h1>Task for the day</h1>
-        <ul>
-            <?php foreach($tasks as $task): ?>
-            <?php if($task->completed == true): ?>
-               <strike>
-                  <li>
-                     <?=$task->description?>
-                  </li>
-               </strike>
-               <?php else: ?>
-                  <li>
-                     <?=$task->description?>
-                  </li>
-               <?php endif; ?> 
-               
-               <?php endforeach; ?> 
-        </ul>
-</body>
-</html>
+<?php require('partials/head.php'); ?>
+</ul>
+<h1>Task for the day</h1>
+<ul>
+   <?php foreach($tasks as $task): ?>
+      <?php if($task->completed == true): ?>
+         <strike>
+            <li>
+               <?=$task->description?>
+            </li>
+         </strike>
+         <?php else: ?>
+            <li>
+               <?=$task->description?>
+            </li>
+            <?php endif; ?> 
+            
+            <?php endforeach; ?> 
+         </ul>
+         <?php require('partials/footer.php'); ?>
+         
  
                                                               
---
+++ views/partials/nav.php
@@ -0,0 +1,13 @@
+<ul>
+      <li>
+         <a href="">Home</a>
+      </li>
+      <li>
+         <a href="/about">About</a>
+      </li>
+      <li>
+         <a href="/about/culture">About Our Culture</a>
+      </li>
+      <li>
+         <a href="/contact">Contact</a>
+      </li>
